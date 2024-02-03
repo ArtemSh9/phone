4ffd8b123c349373dd816bddcf8fecde2e0b440b
Artem <exampl12@yandex.ru>
2024-02-03 16:00:25 +0300

    copy

--- phone.py
+++ phone.py
@@ -15,6 +15,13 @@ def search_user(data: str, filename: str) ->str:
         res = [item for item in text if data.lower() in item.lower()]
     return ''.join(res) if res else 'Вхождений не найдено'
 
+def copy(filename: str):
+    with open(filename, 'r', encoding='utf-8') as fr, open(write_file, 'a', encoding='utf-8') as fw:
+        for i in fr:
+            identify = i.split(";")[0]
+            if identify == id:
+                fw.write(i)
+
 def check_directory(filename: str):
     if filename not in os.listdir():
         with open(filename, 'w', encoding='utf-8') as data:
@@ -24,8 +31,10 @@ INFO_STRING = """
 1 - вывести все данные
 2 - добавление нового пользователя
 3 - поиск  
+4 - копирование контакта в другой файл
 """
 DATASOURCE = 'phone.txt'
+write_file = 'newphone.txt'
 check_directory(DATASOURCE)
 while True:
     mode = int(input(INFO_STRING))
@@ -38,3 +47,6 @@ while True:
     elif mode == 3:
         search = input()
         print(search_user(search, DATASOURCE))
+    elif mode == 4:
+        id = input()
+        copy(DATASOURCE)
